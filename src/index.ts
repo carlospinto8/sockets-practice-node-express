c4000abeef1801e53a39359620826bbf774d41c1
Carlos Pinto <123394002+carlospinto8@users.noreply.github.com>
2025-03-07 22:56:54 -0600

    feat: [SOCKETDEV-2] testing serving entire directory using middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 
 const express = require('express');
 const dotenv = require('dotenv');
@@ -7,12 +7,23 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const path = require('path');
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to root URL');
-
+app.post('/', (req: Request, res: Response) => {
+  const { name } = req.body;
+  res.send(`Welcome ${name}`);
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+app.get('/hello', (req: Request, res: Response) => {
+  res.status(200).send('Welcome to root URL');
+});
+
+app.use('/static', express.static(path.join(__dirname, '../static-files')));
+
+app.listen(port, (error: Error) => {
+  if (!error) {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+  } else {
+    console.log('An Error occured, server cannot start', error);
+  }
 });
