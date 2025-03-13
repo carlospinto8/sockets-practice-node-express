0bd7b59f1157b183c2dd19317058e85fc52ab58a
Carlos Pinto <123394002+carlospinto8@users.noreply.github.com>
2025-03-12 23:33:49 -0500

    feat: [SOCKETDEV-3] implemented a ping-pong heartbeat

--- public/js/app.ts
+++ public/js/app.ts
@@ -1,6 +1,12 @@
 (
     function() {
-        let ws: WebSocket = new WebSocket('http://localhost:3000') ;
+        interface WebSocketExt extends WebSocket {
+            pingTimeOut: NodeJS.Timeout;
+        }
+
+        let ws: WebSocketExt;
+        const HEARTBEAT_TIMEOUT = ((1000 * 5) + (1000 * 1));
+        const HEARTBEAT_VALUE = 1;
         const messages = <HTMLElement>document.getElementById('messages');
         const wsOpen = <HTMLButtonElement>document.getElementById('ws-open');
         const wsClose = <HTMLButtonElement>document.getElementById('ws-close');
@@ -21,10 +27,34 @@
             }           
         }
 
+        function heartbeat() {
+            if (!ws) {
+                return;
+            } else if (!!ws.pingTimeOut) {
+                clearTimeout(ws.pingTimeOut);
+            }
+
+            ws.pingTimeOut = setTimeout(() => {
+                ws.close();
+
+                // Business Logic for deciding whether or not to reconnect
+            }, HEARTBEAT_TIMEOUT);
+
+            const data = new Uint8Array(1);
+
+            data[0] = HEARTBEAT_VALUE;
+
+            ws.send(data);
+        }
+
+        function isBinary(obj: any) {
+            return typeof obj === 'object' && Object.prototype.toString.call(obj) ==='[object Blob]';
+        }
+
         wsOpen.addEventListener('click', () => {
             closeConnection();
 
-            ws = new WebSocket('ws://localhost:3000');
+            ws = new WebSocket('ws://localhost:3000') as WebSocketExt;
 
 
             ws.addEventListener('error', () => {
@@ -37,11 +67,19 @@
 
             ws.addEventListener('close', () => {
                 showMessage('WebSocket Connection close');
+
+                if (!!ws.pingTimeOut) {
+                    clearTimeout(ws.pingTimeOut);
+                }
             });
 
             // msg will usually be an object requiring parsing but not right now for learning purposes
             ws.addEventListener('message', (msg: MessageEvent<string>) => {
-                showMessage(`Received Message: ${msg.data}`);
+                if (isBinary(msg.data)) {
+                    heartbeat();
+                } else {
+                    showMessage(`Received message: ${msg.data}`);
+                }
             });
         });
 
--- sockets/socket-index.ts
+++ sockets/socket-index.ts
@@ -1,5 +1,17 @@
-import { Server } from 'http';
+import { IncomingMessage, Server } from 'http';
+import { Socket } from 'net';
 import { WebSocketServer, WebSocket } from 'ws';
+
+const HEARTBEAT_INTERVAL = 1000 * 5; // In prod, use a bigger interval like 10-30 seconds
+const HEARTBEAT_VALUE = 1;
+
+interface ExtendedWebSocket extends WebSocket {
+    isAlive: boolean;
+  }
+
+function ping(ws: WebSocket) {
+    ws.send(HEARTBEAT_VALUE, { binary: true });
+}
 
 // HTTP server is error handling
 function onSocketPreError(e: Error) {
@@ -13,9 +25,10 @@ function onSocketPostError(e: Error) {
 }
 
 export default function configureSockets(s: Server) {
+
     const wss: WebSocketServer = new WebSocketServer({ noServer: true });
 
-    s.on('upgrade', (req: any, socket: any, head: any) => {
+    s.on('upgrade', (req: IncomingMessage, socket: Socket, head: Buffer) => {
         // Before officially upgraded and emitted to the server
         socket.on('error', onSocketPreError);
 
@@ -29,20 +42,49 @@ export default function configureSockets(s: Server) {
         });
     });
 
-    wss.on('connection', (ws, req) => {
-        ws.on('error', onSocketPostError);
+    wss.on('connection', (ws: WebSocket, req) => {
+        const extWs = ws as ExtendedWebSocket;
 
-        ws.on('message', (msg, isBinary) => {
-            wss.clients.forEach((client) => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(msg, { binary: isBinary });
-                }
-            });
+        extWs.isAlive = true;
+
+        extWs.on('error', onSocketPostError);
+
+        extWs.on('message', (msg, isBinary) => {
+            if (isBinary && (msg as any)[0] === HEARTBEAT_VALUE) {
+                extWs.isAlive = true;
+            } else {
+                wss.clients.forEach((client) => {
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(msg, { binary: isBinary });
+                    }
+                });
+            }
+
+
         });
 
-        ws.on('close', () => {
+        extWs.on('close', () => {
             console.log('Connection closed');
         })
 
     });
+
+    const interval = setInterval(() => {
+        console.log('firing interval');
+        wss.clients.forEach((client) => {
+            const extClient = client as ExtendedWebSocket;
+
+            if (!extClient.isAlive) {
+                client.terminate();
+                return;
+            }
+
+            extClient.isAlive = false;
+            ping(extClient);
+        });
+    }, HEARTBEAT_INTERVAL);
+
+    wss.on('close', () => {
+        clearInterval(interval);
+    })
 }
